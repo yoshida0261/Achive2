bed0056fd30efe56235266875c1d0794e78674ce
yoshida0261 <yoshida261@gmail.com>
2016-10-29 03:17:21 +0900

    utilクラス作成

---
+++ app/src/main/java/com/starcompany/achive2/util/AlearmUtil.java
@@ -0,0 +1,8 @@
+package com.starcompany.achive2.util;
+
+/**
+ * Created by takeshi on 16/10/29.
+ */
+public class AlearmUtil {
+    //todo 一日のまとめ
+}
---
+++ app/src/main/java/com/starcompany/achive2/util/DateUtil.java
@@ -0,0 +1,7 @@
+package com.starcompany.achive2.util;
+
+/**
+ * Created by takeshi on 16/10/29.
+ */
+public class DateUtil {
+}
