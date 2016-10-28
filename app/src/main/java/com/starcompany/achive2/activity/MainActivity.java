09e5c02ce80940f3a98566bdb55c95e7818fe223
yoshida0261 <yoshida261@gmail.com>
2016-10-29 03:02:15 +0900

    ・directory作成 ・DI用componentを追加

--- app/src/main/java/com/starcompany/achive2/activity/MainActivity.java
+++ app/src/main/java/com/starcompany/achive2/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.starcompany.achive2;
+package com.starcompany.achive2.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.starcompany.achive2.R;
 
 public class MainActivity extends AppCompatActivity {
 
