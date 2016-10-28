f98668d14ed6dafb725699cb67ae44c8756bc5ad
yoshida0261 <yoshida261@gmail.com>
2016-10-29 04:45:15 +0900

    dateを追加 付随してlocaleメソッドを追加

--- app/src/main/java/com/starcompany/achive2/util/DateUtil.java
+++ app/src/main/java/com/starcompany/achive2/util/DateUtil.java
@@ -1,7 +1,69 @@
 package com.starcompany.achive2.util;
 
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.text.format.DateFormat;
+import android.text.format.DateUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by takeshi on 16/10/29.
  */
 public class DateUtil {
+    private static final String FORMAT_MMDD = "MMMd";
+    private static final String FORMAT_KKMM = "kk:mm";
+    private static final String FORMAT_YYYYMMDDKKMM = "yyyyMMMdkkmm";
+
+    @NonNull
+    public static String getMonthDate(Date date, Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            String pattern = DateFormat.getBestDateTimePattern(Locale.getDefault(), FORMAT_MMDD);
+            return new SimpleDateFormat(pattern).format(date);
+        } else {
+            int flag = DateUtils.FORMAT_ABBREV_ALL | DateUtils.FORMAT_NO_YEAR;
+            return DateUtils.formatDateTime(context, date.getTime(), flag);
+        }
+    }
+
+    @NonNull
+    public static String getHourMinute(Date date) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            String pattern = DateFormat.getBestDateTimePattern(Locale.getDefault(), FORMAT_KKMM);
+            return new SimpleDateFormat(pattern).format(date);
+        } else {
+            return String.valueOf(DateFormat.format(FORMAT_KKMM, date));
+        }
+    }
+
+    @NonNull
+    public static String getLongFormatDate(Date date, Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            String pattern = DateFormat.getBestDateTimePattern(Locale.getDefault(), FORMAT_YYYYMMDDKKMM);
+            return new SimpleDateFormat(pattern).format(date);
+        } else {
+            java.text.DateFormat dayOfWeekFormat = java.text.DateFormat.getDateInstance(java.text.DateFormat.LONG);
+            java.text.DateFormat shortTimeFormat = java.text.DateFormat.getTimeInstance(java.text.DateFormat.SHORT);
+
+            //todo local
+            // dayOfWeekFormat.setTimeZone(LocaleUtil.getDisplayTimeZone(context));
+           // shortTimeFormat.setTimeZone(LocaleUtil.getDisplayTimeZone(context));
+            return dayOfWeekFormat.format(date) + " " + shortTimeFormat.format(date);
+        }
+    }
+
+    public static int getMinutes(Date stime, Date etime) {
+        long range = etime.getTime() - stime.getTime();
+
+        if (range > 0) {
+            return (int) (range / TimeUnit.MINUTES.toMillis(1L));
+        } else {
+            return 0;
+        }
+    }
+
 }
--- app/src/main/java/com/starcompany/achive2/util/LocaleUtil.java
+++ app/src/main/java/com/starcompany/achive2/util/LocaleUtil.java
@@ -1,7 +1,33 @@
 package com.starcompany.achive2.util;
 
+import android.content.Context;
+
+import com.starcompany.achive2.BuildConfig;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.TimeZone;
+
 /**
  * Created by takeshi on 16/10/29.
  */
 public class LocaleUtil {
+    public static final String LANG_EN_ID = "en";
+    public static final String LANG_JA_ID = "ja";
+    public static final String LANG_AR_ID = "ar";
+    public static final String LANG_KO_ID = "ko";
+    public static final List<String> SUPPORT_LANG = Arrays.asList(LANG_EN_ID, LANG_JA_ID, LANG_AR_ID, LANG_KO_ID);
+
+    private static final String TAG = LocaleUtil.class.getSimpleName();
+
+    // todo build config
+    //private static final TimeZone CONFERENCE_TIMEZONE = TimeZone.getTimeZone(BuildConfig.);
+
+/*
+    public static TimeZone getDisplayTimeZone(Context context) {
+        TimeZone defaultTimeZone = TimeZone.getDefault();
+        boolean shouldShowLocalTime = DefaultPrefs.get(context).getShowLocalTimeFlag();
+        return (shouldShowLocalTime && defaultTimeZone != null) ? defaultTimeZone : CONFERENCE_TIMEZONE;
+    }
+*/
 }
