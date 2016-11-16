c5c6b4a1e8dcc0bc5efec6309a19157f2d43c775
yoshida0261 <yoshida261@gmail.com>
2016-11-17 02:10:17 +0900

    build出来て実行可能になった

--- app/src/main/java/com/starcompany/achive2/activity/MainActivity.java
+++ app/src/main/java/com/starcompany/achive2/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.starcompany.achive2.activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,13 @@ import com.starcompany.achive2.R;
 
 public class MainActivity extends AppCompatActivity {
 
+//    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+/*
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);*/
     }
 }
